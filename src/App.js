50c3c38a53b6778d21ff7885204a83cd2cc1bf86
valwebd <77312053+valwebd@users.noreply.github.com>
2021-10-06 19:10:03 -0700

    convert-App.js-to-functional-component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,37 @@
-import React,{ Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CardList } from './components/card-list/card-list.component';
-import { SearchBox } from "./components/search-box/search-box.component.jsx";
+import { SearchBox } from './components/search-box/search-box.component.jsx';
 import './App.css';
 
-class App extends Component{
-  constructor(){
-    super();
+const App = () => {
+  const [monsters, setMonsters] = useState([]);
+  const [searchField, setSearchField] = useState('');
 
-    this.state = {
-      monsters: [],
-      searchField: '',
-    };
-
-  }
-
-  // this is not predefined in the scope of functions in js nut the arrow function binds code without using any other command
-  OnSearchChange = (event) => {
-    this.setState({ searchField: event.target.value })
-  }
-
-  componentDidMount(){
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ monsters: users }));
-  }
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
 
-  render(){
-    // takes our the states and puts them inconstants with same name 
-    // just like const monsters = this.state.monsters;
-    const { monsters, searchField} = this.state; 
-    const filteredMonsters = monsters.filter(monster => 
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
-    );
-    return(
-          <div className="App">
-            <h1>Monsters Rolodex</h1>
-            <SearchBox 
-              placeholder = 'search monsters'
-              OnSearchChange = {this.OnSearchChange}
-            />
-            <CardList monsters={filteredMonsters} />
-          </div>
-        );
-      }
-  }
+  const filteredMonsters = monsters.filter((monster) =>
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+  );
+
+  const OnSearchChange = (e) => {
+    setSearchField(e.target.value);
+  };
+
+  return (
+    <div className='App'>
+      <h1>Monsters Rolodex</h1>
+
+      <SearchBox
+        placeholder='search monsters'
+        OnSearchChange={OnSearchChange}
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
 
 export default App;
