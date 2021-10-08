30e0890317836db6ff1c1ffb45015096d3316734
sandypswami <swami.21sandeep@gmail.com>
2021-10-08 22:45:21 +0530

    Added Favicon , title and added search icon

--- src/App.js
+++ src/App.js
@@ -1,48 +1,54 @@
-import React,{ Component } from 'react';
-import { CardList } from './components/card-list/card-list.component';
+import React, { Component } from "react";
+import { IconButton } from "@material-ui/core";
+import { SearchOutlined } from "@material-ui/icons";
+import { CardList } from "./components/card-list/card-list.component";
 import { SearchBox } from "./components/search-box/search-box.component.jsx";
-import './App.css';
+import "./App.css";
 
-class App extends Component{
-  constructor(){
-    super();
 
-    this.state = {
-      monsters: [],
-      searchField: '',
-    };
+class App extends Component {
+	constructor() {
+		super();
 
-  }
+		this.state = {
+			monsters: [],
+			searchField: "",
+		};
+	}
 
-  // this is not predefined in the scope of functions in js nut the arrow function binds code without using any other command
-  OnSearchChange = (event) => {
-    this.setState({ searchField: event.target.value })
-  }
+	// this is not predefined in the scope of functions in js nut the arrow function binds code without using any other command
+	OnSearchChange = (event) => {
+		this.setState({ searchField: event.target.value });
+	};
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ monsters: users }));
-  }
+	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((response) => response.json())
+			.then((users) => this.setState({ monsters: users }));
+	}
 
-  render(){
-    // takes our the states and puts them inconstants with same name 
-    // just like const monsters = this.state.monsters;
-    const { monsters, searchField} = this.state; 
-    const filteredMonsters = monsters.filter(monster => 
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
-    );
-    return(
-          <div className="App">
-            <h1>Monsters Rolodex</h1>
-            <SearchBox 
-              placeholder = 'search monsters'
-              OnSearchChange = {this.OnSearchChange}
-            />
-            <CardList monsters={filteredMonsters} />
-          </div>
-        );
-      }
-  }
+	render() {
+		// takes our the states and puts them inconstants with same name
+		// just like const monsters = this.state.monsters;
+		const { monsters, searchField } = this.state;
+		const filteredMonsters = monsters.filter((monster) =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+		);
+		return (
+			<div className="App">
+				<h1>Monsters Rolodex</h1>
+				<SearchBox
+					className="search_box"
+					placeholder="search monsters"
+					OnSearchChange={this.OnSearchChange}
+				/>
+				<IconButton>
+					<SearchOutlined />
+				</IconButton>
+				<CardList monsters={filteredMonsters} />
+			</div>
+		);
+	}
+}
 
 export default App;
